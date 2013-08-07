7c4224f79f10e2cd7ef3db41d06a6605a4828af8
wevertonsreis <weverton.sant.reis@gmail.com>
2013-08-07 10:39:54 -0300

    Treino.

--- Teste Struts 2/WebContent/register.jsp
+++ Teste Struts 2/WebContent/register.jsp
@@ -6,10 +6,26 @@
 <title>Register</title>
 </head>
 <body>
+	<script type="text/javascript">
+		function save(){
+			document.forms[0].action = "register.do?method=save"
+			document.forms[0].submit();
+		}
+		
+		function del() {
+			document.forms[0].action = "register.do?method=remove"
+			document.forms[0].submit();
+		}
+	</script>
+
+	</head>
+
 	<html:form action="/register">
-		Name<html:text property="name" name="registerForm"></html:text>
-		Fone<html:text property="fone" name="registerForm"></html:text>
-		<html:submit value="Okay"/>
+		Name<html:text property="name" name="registerForm"/>
+		Fone<html:text property="fone" name="registerForm"/>
+		<html:submit value="Remove" onclick="del()"/>
+		<html:submit value="Save" onclick="save()"/>
 	</html:form>
+	
 </body>
 </html>
--- Teste Struts 2/src/com/actions/RegisterAction.java
+++ Teste Struts 2/src/com/actions/RegisterAction.java
@@ -10,12 +10,24 @@ import org.apache.struts.actions.DispatchAction;
 
 public class RegisterAction extends DispatchAction {
 	
+	public ActionForward save(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response) throws Exception {
+		System.out.println("Save");
+		return mapping.getInputForward();
+	}
+	
+	public ActionForward remove(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response) throws Exception {
+		System.out.println("Remove");
+		return mapping.getInputForward();
+	}
+	
 	@Override
 	protected ActionForward unspecified(ActionMapping mapping, ActionForm form,
 			HttpServletRequest request, HttpServletResponse response)
 			throws Exception {
-		// TODO Auto-generated method stub
-		return super.unspecified(mapping, form, request, response);
+		System.out.println("Unspecifield");
+		return mapping.getInputForward();
 	}
 
 }
